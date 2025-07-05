1fd77e0d8a8fed026d90a0b3fc9190cbc43aa9dc
Spud27 <palumbo9673@gmail.com>
2025-07-05 10:37:28 +1000

    added update and delete crud routes

--- models/post.js
+++ models/post.js
@@ -5,9 +5,9 @@ import { model } from 'mongoose';
 // Express is an ODM (Object Document Mapper) for MongoDB
 // it provides a way to define schemas and models for MongoDB documents
 const Post = model('Post', {
-    title: String,
-    body: String,
-    isPublished: Boolean
+    title: {type: String, required: true},
+    body: {type: String, required: true},
+    isPublished: {type: Boolean, default: false},
 });
 
 export default Post;
--- routes/post_routes.js
+++ routes/post_routes.js
@@ -9,12 +9,12 @@ const router = Router()
 // posts routes
 // GET all posts
 router.get('/posts', async (req, res) => {
-  res.send(await Post.find()) // using mongoose to find all posts
+  res.send(await Post.find({ isPublished: true })) // using mongoose to find all posts
 })
 // GET one post
 router.get('/posts/:id', async (req, res) => {
   const postId = parseInt(req.params.id, 10)
-  const post = await Post.find({_id: postId}) // using mongoose to find a post by id
+  const post = await Post.findOne({_id: postId}) // using mongoose to find a post by id
   // if post is found, send it as a response
   
   if (post) {
@@ -24,15 +24,47 @@ router.get('/posts/:id', async (req, res) => {
   }
 })
 // create a new post
-router.post('/posts', (req, res) => {
-    // get post data from the request body
+router.post('/posts', async (req, res) => {
+    try {
+            // get post data from the request body
     const bodyData = req.body
-    // create a new post instance
-    // commit the new post to the database
-    // set the post to the request body
+    // create and save a new post instance
+    const post = await Post.create(bodyData) // using mongoose to create a new post instance
+    // send the post as a response
+    res.status(201).send(post) 
+    }
+    catch (error) {
+        // log the error to an error file
+        res.status(400).send({ error: error.message })
+    }
 })
 // update a post
+async function updatePost(req, res) {
+    // 1. fetch the post from the database using the id from the request params
+    const post = await Post.findByIdAndUpdate(req.params.id, req.body, {returnDocument: 'after'})
+    if (post) {
+        // 2. save the post to the database
+        post.save() // using mongoose to save the post instance
+        // 3. send the updated post as a response
+        res.send(post)
+    } else {
+        // if post is not found, send a 404 response
+        res.status(404).send({ error: `Post with id = ${req.params.id} not found` })
+    }
+}
+
+router.put('/posts/:id', updatePost) // update a post by id
+router.patch('/posts/:id', updatePost) // update a post by id with partial data
+
 // delete a post
+router.delete('/posts/:id', async (req, res) => {
+  const post = await Post.findByIdAndDelete(req.params.id) // using mongoose to delete a post by id
+  if (post) {
+    res.status(204).send(post) // send a 204 response if post is deleted
+  } else {
+    res.status(404).send({ error: `Post with id = ${req.params.id} not found` }) // send a 404 response if post is not found
+  } 
+})
 
 // one default export allowed per module
 // its anonymous
