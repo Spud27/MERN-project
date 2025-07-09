5c13963d3807e3d27e6090b32dfb795cd3b783da
Spud27 <palumbo9673@gmail.com>
2025-07-09 20:36:43 +1000

    auth

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 import express from 'express'
 import post_routes from './routes/post_routes.js'
 import { connect } from 'mongoose'
+import cors from 'cors'
+import helmet from 'helmet'
 
 
 const app = express()
 const port = 3000
+
+// securtity middleware should be used before any other middleware
+app.use(helmet())
+// cors middleware to allow cross-origin requests
+app.use(cors())
 
 // use express.json() middleware to parse JSON request bodies
 app.use(express.json())
